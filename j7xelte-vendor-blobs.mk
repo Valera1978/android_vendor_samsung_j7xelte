d53ab4cec620388fc1c90ee33ce000064b1562fc
Valera1978 <valera1978@tut.by>
2017-12-10 14:07:28 +0300

    add stock libion, some usr files

--- j7xelte-vendor-blobs.mk
+++ j7xelte-vendor-blobs.mk
@@ -133,6 +133,61 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
 
 PRODUCT_COPY_FILES += \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/c_fst:system/usr/srec/en-US/c_fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/dict:system/usr/srec/en-US/dict \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/dnn:system/usr/srec/en-US/dnn \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/endpointer_model:system/usr/srec/en-US/endpointer_model \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/ep_portable_mean_stddev:system/usr/srec/en-US/ep_portable_mean_stddev \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/g2p_fst:system/usr/srec/en-US/g2p_fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/hmm_symbols:system/usr/srec/en-US/hmm_symbols \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/hmmlist:system/usr/srec/en-US/hmmlist \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/input_mean_std_dev:system/usr/srec/en-US/input_mean_std_dev \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/metadata:system/usr/srec/en-US/metadata \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/norm_fst:system/usr/srec/en-US/norm_fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/phonelist:system/usr/srec/en-US/phonelist \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/portable_lstm:system/usr/srec/en-US/portable_lstm \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/portable_meanstddev:system/usr/srec/en-US/portable_meanstddev \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/word_classifier:system/usr/srec/en-US/word_classifier \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/commands.abnf:system/usr/srec/en-US/commands.abnf \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/contacts.abnf:system/usr/srec/en-US/contacts.abnf \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/compile_grammar.config:system/usr/srec/en-US/compile_grammar.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/dictation.config:system/usr/srec/en-US/dictation.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/endpointer_dictation.config:system/usr/srec/en-US/endpointer_dictation.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/endpointer_voicesearch.config:system/usr/srec/en-US/endpointer_voicesearch.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/grammar.config:system/usr/srec/en-US/grammar.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/magic_mic.config:system/usr/srec/en-US/magic_mic.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/voice_actions.config:system/usr/srec/en-US/voice_actions.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/voice_actions_compiler.config:system/usr/srec/en-US/voice_actions_compiler.config \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/g2p.data:system/usr/srec/en-US/g2p.data \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/lstm_model.uint8.data:system/usr/srec/en-US/lstm_model.uint8.data \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/APP_NAME.fst:system/usr/srec/en-US/APP_NAME.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/CLG.prewalk.fst:system/usr/srec/en-US/CLG.prewalk.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/confirmation_bias.fst:system/usr/srec/en-US/confirmation_bias.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/CONTACT_NAME.fst:system/usr/srec/en-US/CONTACT_NAME.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/contacts_bias.fst:system/usr/srec/en-US/contacts_bias.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/contacts_disambig.fst:system/usr/srec/en-US/contacts_disambig.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/lexicon.U.fst:system/usr/srec/en-US/lexicon.U.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/SONG_NAME.fst:system/usr/srec/en-US/SONG_NAME.fst \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/rescoring.fst.louds:system/usr/srec/en-US/rescoring.fst.louds \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/class_normalizer.mfar:system/usr/srec/en-US/class_normalizer.mfar \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/normalizer.mfar:system/usr/srec/en-US/normalizer.mfar \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/offensive_word_normalizer.mfar:system/usr/srec/en-US/offensive_word_normalizer.mfar \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/endpointer_model.mmap:system/usr/srec/en-US/endpointer_model.mmap \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/ep_portable_model.mmap:system/usr/srec/en-US/ep_portable_model.mmap \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/pumpkin.mmap:system/usr/srec/en-US/pumpkin.mmap \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/offline_action_data.pb:system/usr/srec/en-US/offline_action_data.pb \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/config.pumpkin:system/usr/srec/en-US/config.pumpkin \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/monastery_config.pumpkin:system/usr/srec/en-US/monastery_config.pumpkin \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/semantics.pumpkin:system/usr/srec/en-US/semantics.pumpkin \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/APP_NAME.syms:system/usr/srec/en-US/APP_NAME.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/CONTACT_NAME.syms:system/usr/srec/en-US/CONTACT_NAME.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/graphemes.syms:system/usr/srec/en-US/graphemes.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/phonelist.syms:system/usr/srec/en-US/phonelist.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/phonemes.syms:system/usr/srec/en-US/phonemes.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/SONG_NAME.syms:system/usr/srec/en-US/SONG_NAME.syms \
+    vendor/samsung/j7xelte/proprietary/usr/srec/en-US/wordlist.syms:system/usr/srec/en-US/wordlist.syms
+
+PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/j7xelte/proprietary/lib/librilutils.so:system/lib/librilutils.so \
     vendor/samsung/j7xelte/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
@@ -142,3 +197,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/bin/rild:system/bin/rild \
     vendor/samsung/j7xelte/proprietary/lib/libril.so:system/lib/libril.so
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/j7xelte/proprietary/lib/libion_exynos.so:system/lib/libion_exynos.so
